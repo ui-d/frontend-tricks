21f1e1290848563431764b0548ea2f8cd0e4d93b
ui-d <dawiddeveloper@gmail.com>
2021-07-17 17:36:36 +0200

    valueAsDate with date input

--- scripts.js
+++ scripts.js
@@ -1,22 +1,9 @@
-const inputEl = document.querySelector('.inputEl');
-
-const handleVisibility = ({type}) => {
-
-    const [column] = [...document.getElementsByClassName(type)];
-    const label = document.createElement('p')
-    label.classList.add(type);
-    label.classList.add('visible');
-    label.textContent = type;
-    column.append(label);
-
-    setTimeout(() => {
-        label.remove();
-    }, 2000)
-}
+const dateInput = document.querySelector('.inputEl');
+const dateOutput = document.querySelector('.alert');
+      dateInput.valueAsDate = new Date();
 
 
-inputEl.addEventListener('input', handleVisibility)
-
-inputEl.addEventListener('change', handleVisibility)
-
-inputEl.addEventListener('keypress', handleVisibility)
+dateInput.addEventListener('input', () => {
+    // dateOutput. innerText = new Date(dateInput.value);
+    dateOutput. innerText = dateInput.valueAsDate;
+})
